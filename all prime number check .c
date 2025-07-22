6b2135e22c3d157b896d785728dea19f13b9322e
Ujjwal Choubey <ujjwalchoubey09@gmail.com>
2025-07-22 09:47:01 +0530

    Create  all prime number check .c

---
+++ all prime number check .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+  int i,j,n=50;
+int prime=1;
+for(j=2; j<=i/2; J++){
+if(i%j==0){
+prime=0;
+break;
+}
+}
+if(prime)
+  printf("%d",i);
+}
+printf("\n");
+return 0:
+  }
